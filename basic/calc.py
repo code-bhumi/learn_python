c502090261fa54084f723fd51084ac78c10f34be
Meenakshi Pal <pal.meenakshi@outlook.com>
2023-01-17 22:08:43 +1100

    calculator

---
+++ basic/calc.py
@@ -0,0 +1,14 @@
+x = int(input("what's x? "))
+y = int(input("what's y? "))
+
+z = round(x / y, 2)
+
+a = int(input("what's a? "))
+b = int(input("what's b? "))
+
+w = (a + b)
+
+print(f"division of numbers is... {z}")
+
+print(f"addition of numbers is... {w:.2f}")
+

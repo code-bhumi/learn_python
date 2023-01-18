e2e2728174eb086da9405b21f627e0cb398a2947
Meenakshi Pal <pal.meenakshi@outlook.com>
2023-01-18 22:01:24 +1100

    matrix

---
+++ basic/matrix.py
@@ -0,0 +1,16 @@
+def main():
+    print_square(6)
+    
+    
+def print_square(size):
+    for i in range(size):
+        print_row(size)
+        
+def print_row(width):
+    print("&" * width)
+    
+main()  
+    
+        
+
+    

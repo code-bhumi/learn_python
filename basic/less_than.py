57958880fdc50a9cb318eaf9fab493fd9910f7b3
Meenakshi Pal <pal.meenakshi@outlook.com>
2023-01-19 17:45:02 +1100

    update

--- basic/less_than.py
+++ basic/less_than.py
@@ -1,9 +1,10 @@
-def main():
+def test():
     numbers = [5,6,84,9,2,5,34,14,12,54,18,16]
     x = int(input("what is x? ")) 
     
     for number in numbers:
         if number <= x:
-            print(number)
+            print(number, end=', ')
             
-main()            
+if __name__ == '__main__':
+    test()

0f71a36980edfc01bc07f1de85980d66ebc00116
Meenakshi Pal <pal.meenakshi@outlook.com>
2023-01-19 17:37:45 +1100

    less-than a number

---
+++ basic/less_than.py
@@ -0,0 +1,9 @@
+def main():
+    numbers = [5,6,84,9,2,5,34,14,12,54,18,16]
+    x = int(input("what is x? ")) 
+    
+    for number in numbers:
+        if number <= x:
+            print(number)
+            
+main()            

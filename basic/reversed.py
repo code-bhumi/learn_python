c095b95ef547cac560f3f3519e09e52bbe6d1d36
Meenakshi Pal <pal.meenakshi@outlook.com>
2023-03-11 22:39:17 +1100

    special and reverse

---
+++ basic/reversed.py
@@ -0,0 +1,8 @@
+def total():
+    list = [20,45,16,84,95,71,64,89]
+    new_list = reversed(list)
+    for number in new_list:
+        print(number,end=" ")
+    
+if __name__ == '__main__':
+    total()    

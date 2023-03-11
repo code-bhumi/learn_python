cb39374c3a686c5450e3c8b6060c3755f13d0d05
Meenakshi Pal <pal.meenakshi@outlook.com>
2023-03-11 22:50:27 +1100

    pattern

--- basic/times_table.py
+++ basic/times_table.py
@@ -1,8 +1,7 @@
 def times_table():
     num = int(input("which times table do you want me to print..."))
     for i in range(1,11):
-        solution = num * i 
-        print(solution)
+        print(f'{num} x {i} == {num * i}')
 
 if __name__ == '__main__':
     times_table()      
